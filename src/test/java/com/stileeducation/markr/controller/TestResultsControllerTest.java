e120a6d1a9e779cdcb1d293245f55f296e77e91a
Szymon Szukalski <szymon@skas.io>
2024-07-23 16:33:15 +1000

    Remove unused variable

--- src/test/java/com/stileeducation/markr/controller/TestResultsControllerTest.java
+++ src/test/java/com/stileeducation/markr/controller/TestResultsControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.*;
 
 import static com.stileeducation.markr.controller.TestResultsController.IMPORT_ENDPOINT;
@@ -15,9 +14,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TestResultsControllerTest {
-
-  @LocalServerPort
-  private int port;
 
   @Autowired
   private XmlMapper xmlMapper;
